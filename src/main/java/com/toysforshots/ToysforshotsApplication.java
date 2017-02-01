15b7f42cf281f529ee5ec93c9f838893c14dfb84
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2017-02-01 17:49:59 -0500

    Thymleaf stuff

--- src/main/java/com/toysforshots/ToysforshotsApplication.java
+++ src/main/java/com/toysforshots/ToysforshotsApplication.java
@@ -8,5 +8,6 @@ public class ToysforshotsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToysforshotsApplication.class, args);
+		System.out.println("Running");
 	}
 }
--- src/main/java/com/toysforshots/controller/PageController.java
+++ src/main/java/com/toysforshots/controller/PageController.java
@@ -1,14 +1,14 @@
 package com.toysforshots.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PageController {
-
+	
 	@RequestMapping("/")
 	public String home(){
-		return "Hello World!";
+		return "index";
 	}
-	
+
 }
